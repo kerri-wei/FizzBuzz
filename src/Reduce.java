977352901c2bb9ea104e4f7c43a0d015a01ca4ab
kerri-wei <kkerriwei@gmail.com>
2024-09-10 15:40:47 -0400

    copmleted code for Reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int num = 100;
+        int reductions = 0;
+
+        do{
+            if(num % 2 == 0){
+                num = num / 2;
+            }
+            else{
+                num = num - 1;
+            }
+            reductions++;
+        }while(num > 0);
+
+        System.out.println(reductions);
+    }
 }
