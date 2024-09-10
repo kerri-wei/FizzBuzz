f05d1d9c88a929ddcc0a4894d84e3d2bdf27ab80
kerri-wei <kkerriwei@gmail.com>
2024-09-10 15:34:20 -0400

    added method to multiples

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,5 +1,18 @@
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println("333");
+        System.out.println("There are " + multiples(1000, 3) + " multiples of 3 and " + multiples(1000, 5) + " multiples of 5.");
+    }
+
+    public static int multiples(int n, int a) {
+        int a_multiples = 0;
+        int a_so_far = a;
+
+        do {
+            a_multiples++;
+            a_so_far = a_so_far + a;
+        } while (a_so_far <= n);
+
+        return a_multiples;
+
     }
 }
